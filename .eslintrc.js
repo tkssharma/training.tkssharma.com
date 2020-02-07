190c58bc45171bc9e750b9907a896a8730f1e3e6
tkssharma <tarun.softengg@gmail.com>
2020-02-07 14:20:00 +0530

    updated eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,36 @@ module.exports = {
     es6: true,
     mocha: true,
   },
-  parser: "babel-eslint",
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'], // extending recommended config and config derived from eslint-config-prettier
+  parser: 'babel-eslint',
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  // extending recommended config and config derived from eslint-config-prettier
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 7,
     allowImportExportEverywhere: true,
     ecmaFeatures: {
       jsx: true,
-      "objectLiteralDuplicateProperties": false,
-      "experimentalObjectRestSpread": true
+      objectLiteralDuplicateProperties: false,
+      experimentalObjectRestSpread: true,
     },
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['prettier', 'react', 'import', 'jsx-a11y', 'react-hooks'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   rules: {
-    "react/prop-types": 0,
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'no-console': 'warn',
+    'react/prop-types': 0,
     'prettier/prettier': [
       'error',
       {
